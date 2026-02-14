ca16e30dc2f9dd8dfa0dceacacecbc079262404f
punkyDeveloper <167206991+punkyDeveloper@users.noreply.github.com>
2026-02-13 22:54:47 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}login`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
